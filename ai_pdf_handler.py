aa2cee2fec44aeddf377dc65d3704c53b695ac77
RealUltra <mirzarameezahmedbaig@gmail.com>
2025-03-01 10:12:43 -0800

    First commit to ai_pdf_handler branch.

---
+++ ai_pdf_handler.py
@@ -0,0 +1,13 @@
+def generate_content(pdf_file: str):
+    '''
+    :param pdf_file: File path of the pdf file that will be used to generate content from.
+    :return: Must return a list in the following format:
+    [
+        (1, "Hello! I'm Person 1"),
+        (2, "And I'm Person 2!"),
+        (1, "Person 1 again!!")
+    ]
+    '''
+
+    return []
+
