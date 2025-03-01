52af592633907a82b1a6b558eaa5e27ae0e40482
RealUltra <mirzarameezahmedbaig@gmail.com>
2025-03-01 11:18:43 -0800

    updated template

--- video_generator.py
+++ video_generator.py
@@ -1,10 +1,12 @@
-def generate_video(content, video_file):
+def generate_video(content, bg_video_file, output_file):
     '''
     :param content: [
         (1, "Hello! I'm Person 1"),
         (2, "And I'm Person 2!"),
         (1, "Person 1 again!!")
     ]
+    :param bg_video_file: The file path to the background video.
+    :param output_file: The file path where the output video will be saved.
     :return: None
     '''
     pass
