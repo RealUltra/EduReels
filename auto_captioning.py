061e2fae162385563bfd0d0dfa5b5cafcb11c34d
RealUltra <mirzarameezahmedbaig@gmail.com>
2025-03-01 10:14:35 -0800

    First commit to video_generator branch.

---
+++ auto_captioning.py
@@ -0,0 +1,8 @@
+def add_auto_captions(video_file: str, output_file: str):
+    '''
+    :param video_file: File path of the video you want to add captions to.
+    :param output_file: File path where the edited video should be saved.
+    :return: None
+    '''
+    pass
+
---
+++ video_generator.py
@@ -0,0 +1,10 @@
+def generate_video(content, video_file):
+    '''
+    :param content: [
+        (1, "Hello! I'm Person 1"),
+        (2, "And I'm Person 2!"),
+        (1, "Person 1 again!!")
+    ]
+    :return: None
+    '''
+    pass
