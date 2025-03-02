68560daa8c1b95b15dbff7cb33fdd60fdd706727
RealUltra <mirzarameezahmedbaig@gmail.com>
2025-03-01 16:15:35 -0800

    Removed auto_captioning.py as it is integrated within video_generator.py

--- auto_captioning.py
+++
@@ -1,8 +0,0 @@
-def add_auto_captions(video_file: str, output_file: str):
-    '''
-    :param video_file: File path of the video you want to add captions to.
-    :param output_file: File path where the edited video should be saved.
-    :return: None
-    '''
-    pass
-
